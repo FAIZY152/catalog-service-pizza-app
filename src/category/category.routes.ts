9bafe9578c023bf4a856ea8ffb5bba8499704e20
Muhammad15246 <faizybangash05@gmail.com>
2025-07-01 11:30:43 +0500

    role-based-access

--- src/category/category.routes.ts
+++ src/category/category.routes.ts
@@ -5,6 +5,8 @@ import { CategoryService } from "./category.service";
 import logger from "../config/logger";
 import AsyncHandler from "../utils/AsyncWrapper";
 import Authenticate from "../common/middlewares/Authenticate";
+import { canAccess } from "../common/middlewares/canAccess";
+import { Roles } from "../utils/constants";
 
 const router = express.Router();
 
@@ -19,6 +21,7 @@ router.post(
     "/create",
 
     Authenticate,
+    canAccess([Roles.ADMIN]),
     createCategoryValidation,
     AsyncHandler(categoryControll.create),
 );
